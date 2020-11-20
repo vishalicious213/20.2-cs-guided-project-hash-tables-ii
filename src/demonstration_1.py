71817d1eb168ca8d1222d3107b82e833c3f23dae
Vish Singh <vishalicious213@gmail.com>
2020-11-19 20:31:09 -0800

    added solutions from class

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -1,3 +1,5 @@
+# Recording for lecture: https://youtu.be/Q-39hT-nMHI
+
 """
 You are given a non-empty list of words.
 
@@ -53,3 +55,57 @@ def top_k_frequent(words, k):
     """
     # Your code here
 
+    # UNDERSTAND
+    # words only contain lowercase letters, no weird numbers or symbols
+    # input: words (list of strings) and k (int)
+    # -- we want to return the k most frequent words (so if k = 2, return the top 2 most frequent words)
+    # output: list of strings
+​
+    # PLAN
+    # we could potentially use python's list.count() --> O(n^2)
+        # list.count(x) returns the number of times x appears in the list
+        # so we would have to iterate through the list, and do list.count on almost every element in the list
+​
+    # we want to know the total number of times each word appears
+    # use a dictionary to store the words as keys and number of appearances as values
+    dictionary = {}
+    # iterate through words
+    for word in words:
+        # if the word is in the dictionary, increment the count
+        if word in dictionary:
+            dictionary[word] += 1
+        # if not, add it and set it to 1
+        else:
+            dictionary[word] = 1
+​
+    print(dictionary)
+​
+    # iterate over the values and return the keys for the top k (alphabetically for ties)
+    # option 1: sort the keys based on the values
+    sorted_keys = sorted(dictionary, key=lambda x: (-dictionary[x], x))
+    print(sorted_keys)
+    # return sorted(sorted_keys)
+​
+    return sorted_keys[:k]
+​
+    # counts_to_words = {}
+    # for word, count in dictionary.items():
+    #     if count in counts_to_words:
+    #         counts_to_words[count].append(word)
+    #     else:
+    #         counts_to_words[count] = [word]
+    # print(counts_to_words)
+    # sorted_counts = sorted(counts_to_words.keys(), reverse=True)
+    # print(sorted_counts)
+    # top_k_frequent = []
+    # for count in sorted_counts:
+    #     sorted_words = sorted(counts_to_words[count])
+    #     top_k_frequent.extend(sorted_words)
+    #     if len(top_k_frequent) >= k:
+    #         return top_k_frequent[0:k]
+​
+​
+    # option 2: get max() k times and remove from hash table.
+​
+​
+print(top_k_frequent(["the", "sky", "is", "cloudy", "the", "the", "the", "is", "is"], 4))
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -53,4 +53,37 @@ def frequency_sort(s: str) -> str:
     """
     # Your code here
 
-    
+    # UNDERSTAND
+​
+    # Plan:
+    # use a dictionary to store characters --> count
+    # iterate through the string, adding the differents / incrementing the count
+    dictionary = {}
+    for char in s:  # O(n)
+        if char not in dictionary:
+            dictionary[char] = 1
+        else:
+            count = dictionary[char]
+            dictionary[char] = count + 1
+​
+    # sort the keys in descending order (of the value)
+    sorted_keys = sorted(dictionary, key=dictionary.get, reverse=True)  # O(n log n)
+    print(sorted_keys)
+    # create a collector string
+    collector_string = []
+    # iterating through the sorted keys
+    for char in sorted_keys:  # O(n)
+    #   add that val number of chars to the string
+        count = dictionary[char]
+    #   for example, if you had {"c": 3}, add "ccc" to the string
+    #     for i in range(count):
+    #         new_string += char
+        print(char * count)
+        chars = char * count  # if char = c, and count = 3, "ccc"
+        collector_string.append(chars)  # add chars to the end of the list
+        print(collector_string)
+​
+    # return the collector string
+    return "".join(collector_string)
+​
+print(frequency_sort("free"))
